0aa744408908d9bf97d2675aa4b7be279d7b66e9
Liju <koshyliju8@gmail.com>
2022-01-25 21:36:00 +0530

    Added fn headers

--- math1.py
+++ math1.py
@@ -1,11 +1,15 @@
+#ADD Implementation
 def add(x,y):
 	pass
 
+#SUBTRACT Implementation
 def subtract(x,y):
 	pass
 
+#MULTIPLY Implementation
 def multiply(x,y):
 	pass
 
+#DIVIDE Implementation
 def divide(x,y):
 	pass
